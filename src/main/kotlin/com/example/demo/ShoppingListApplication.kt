b282aeca0f2b3d24f4b13005e5fef6d1dc294c1e
Mariusz Staniak <mariusz.staniak@gmail.com>
2022-06-26 22:07:15 +0200

    add missing annotation

--- src/main/kotlin/com/example/demo/ShoppingListApplication.kt
+++ src/main/kotlin/com/example/demo/ShoppingListApplication.kt
@@ -2,9 +2,11 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 
 @SpringBootApplication
+@EnableScheduling
 class ShoppingListApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/example/demo/service/AutomaticInvoiceService.kt
+++ src/main/kotlin/com/example/demo/service/AutomaticInvoiceService.kt
@@ -3,12 +3,12 @@ package com.example.demo.service
 import com.example.demo.repository.InvoiceRepository
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Component
 
-@Service
+@Component
 class AutomaticInvoiceService(private val invoiceRepository: InvoiceRepository) {
 
-    @Scheduled(initialDelay = 500)
+    @Scheduled(initialDelay = 5000, fixedRate = 1000 * 60 * 60 * 24)
     fun createAutomaticInvoice() {
         val logger = LoggerFactory.getLogger(this.javaClass)
         logger.info("Execute Auto Invoice procedure")
