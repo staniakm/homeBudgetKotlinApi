d59cffbbd6e92da565745b1843bb4e91194425f6
mstaniak <mariusz.staniak@gmail.com>
2023-05-27 10:51:09 +0200

    prepare tests for assortment management

---
+++ src/test/kotlin/com/example/demo/controller/AssortmentControllerTest.kt
@@ -0,0 +1,43 @@
+package com.example.demo.controller
+
+import com.example.demo.IntegrationTest
+import org.junit.jupiter.api.Test
+
+class AssortmentControllerTest : IntegrationTest() {
+
+    @Test
+    fun `should get list of assortments for given category and month with expense summary`() {
+
+    }
+
+    @Test
+    fun `should get list of assortments for given category and month and skip assortments with zero expense on it`() {
+
+    }
+
+    @Test
+    fun `should change assortment category`() {
+
+    }
+
+    @Test
+    fun `should return bad request when destination category not exists`() {
+
+    }
+
+    @Test
+    fun `should merge two assortments into one with recalculation of expenses`() {
+
+    }
+
+    @Test
+    fun `should return bad request when destination assortment not exists`() {
+
+    }
+
+    @Test
+    fun `should return bad request when source assortment not exists`() {
+
+    }
+
+}
